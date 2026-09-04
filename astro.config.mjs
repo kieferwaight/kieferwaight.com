519e97b366db2c9b61c6ea0bb6043451fd381b20
Kiefer Waight <kiefer.waight@icloud.com>
2026-09-03 21:05:51 -0500

    Generate responsive variants for R2 images

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://kieferwaight.com',
   integrations: [sitemap()],
+  image: {
+    layout: 'constrained',
+    remotePatterns: [{ protocol: 'https', hostname: 'images.kieferwaight.com' }],
+  },
   markdown: {
     shikiConfig: { theme: 'github-dark-default' },
     syntaxHighlight: 'shiki',
